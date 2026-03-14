5cc113a555f4ae1647bbe2d1dcaeed1e744594e9
RamprasadX1 <23r01a05x1@cmrithyderabad.edu.in>
2026-03-15 01:24:05 +0530

    remove tempCodeRunnerFile

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-def load_data(df):
-    engine=create_engine("postgresql://ram:1234@localhost:5432/rtcpp")
-    df=df.rename(columns={"timestamp":"time_stamp"})
-    print(df)
-    df.to_sql("fact_crypto_prices",engine,if_exists="append",index=False)
-    print("load completed")
